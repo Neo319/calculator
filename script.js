07e0b09c807566b135b68029c2d2bfaf7cdff831
Neo319 <alexmnevins@gmail.com>
2023-11-06 11:32:00 -0500

    create basic operator functions

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log ("here");
 
+function add (x, y) {return x + y};
+function sub (x, y) {return x - y};
+function mult (x, y) {return x * y};
+function divide (x, y) {return x / y};
+
+console.log(sub (100, 2))
+console.log (mult((sub (100, 2)), 2))
+
